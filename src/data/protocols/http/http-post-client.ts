df0abcec86fae6c7463f6c1d6b0a416680769aeb
Jorge Zupirolli <jorgebertolo@gmail.com>
2021-07-27 16:15:36 -0400

    refactor: add generic params to HttpPostClient

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,10 +1,10 @@
 import { HttpResponse } from './http-response'
 
-export type HttpPostParams = {
+export type HttpPostParams<T> = {
   url: string
-  body?: object
+  body?: T
 }
 
-export interface HttpPostClient {
-  post (post: HttpPostParams): Promise<HttpResponse>
+export interface HttpPostClient<T, R> {
+  post (post: HttpPostParams<T>): Promise<HttpResponse<R>>
 }
