5ec4b4b7bec9af5b9fc435fc3eb0bdacbf02a83e
Jorge Zupirolli <jorgebertolo@gmail.com>
2021-10-01 09:41:37 -0700

    feat: ensure ValidationBuilder has a required helper

---
+++ src/validation/validators/index.ts
@@ -0,0 +1,6 @@
+/* eslint-disable no-multiple-empty-lines */
+export * from './email/email-validation'
+export * from './min-length/min-length-validation'
+export * from './required-field/required-field-validation'
+export * from './validation-composite/validation-composite'
+
